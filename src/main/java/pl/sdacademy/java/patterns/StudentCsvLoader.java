cbb69569239b72c7afacf138c9b2d9ce6b2de4a6
Tomasz Kocyla <kocyla.t@gmail.com>
2022-11-20 15:54:11 +0100

    StudentCsvLoader commit

---
+++ src/main/java/pl/sdacademy/java/patterns/StudentCsvLoader.java
@@ -0,0 +1,46 @@
+package pl.sdacademy.java.patterns;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+public class StudentCsvLoader {
+    private final Supplier<InputStream> inputStreamSupplier; //supplier pozwala na dostarczenie obiektu, który nas interesuje
+
+    public StudentCsvLoader(Supplier<InputStream> inputStreamSupplier) {
+        this.inputStreamSupplier = inputStreamSupplier;
+    }
+
+    public List<Student> loadStudents() {
+
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(
+                        inputStreamSupplier.get())
+        )) {
+            return bufferedReader.lines()
+                    .map(line -> line.split(","))
+                    .map(this::parseLine)
+                    .toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private Student parseLine(String[] fields) {
+        return Student.builder()
+                .groupCode(fields[3])
+                .person(Person.builder()
+                        .uuid(UUID.fromString(fields[0]))
+                        .firstName(fields[2])
+                        .lastName(fields[1])
+                        .build()
+                )
+                .build();
+
+    }
+}
---
+++ src/test/java/pl/sdacademy/java/patterns/StudentCsvLoaderTest.java
@@ -0,0 +1,35 @@
+package pl.sdacademy.java.patterns;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StudentCsvLoaderTest {
+    @Test
+    void shouldLoadStudents() {
+        //Given
+        StudentCsvLoader studentCsvLoader = new StudentCsvLoader(() -> StudentCsvLoaderTest.class.getResourceAsStream("/students.csv"));
+
+        //when
+        List<Student> students = studentCsvLoader.loadStudents();
+
+        //then
+        assertThat(students).hasSize(2);
+        assertThat(students.get(0).getGroupCode()).isEqualTo("4A");
+        assertThat(students.get(0).getPerson().getUuid().toString())
+                .isEqualToIgnoringCase("B2CF3A44-FB12-4764-B386-7BFD5EF2C1FC");
+        assertThat(students.get(0).getPerson().getFirstName()).isEqualTo("Anna");
+        assertThat(students.get(0).getPerson().getLastName()).isEqualTo("Kowalska");
+
+        assertThat(students.get(1).getGroupCode()).isEqualTo("4A");
+        assertThat(students.get(1).getPerson().getUuid().toString())
+                .isEqualToIgnoringCase("89130168-5EE5-4C34-988C-1287E17C6D73");
+        assertThat(students.get(1).getPerson().getFirstName()).isEqualTo("Jan");
+        assertThat(students.get(1).getPerson().getLastName()).isEqualTo("Nowak");
+    }
+
+
+}
+
