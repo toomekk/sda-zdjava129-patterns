591a046a11ae214c588b5b5b8438ebca3261fa70
Tomasz Majbaum <tomasz.majbaum@gmail.com>
2022-11-26 15:51:06 +0100

    Good export implementation

--- src/main/java/pl/sdacademy/java/patterns/grade/domain/GradeExportService.java
+++ src/main/java/pl/sdacademy/java/patterns/grade/domain/GradeExportService.java
@@ -1,50 +1,32 @@
 package pl.sdacademy.java.patterns.grade.domain;
 
-import pl.sdacademy.java.external.CsvWriter;
-import pl.sdacademy.java.external.ToStringGenerator;
-
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.List;
 import java.util.UUID;
 
 public class GradeExportService {
     private final GradeRepository gradeRepository;
+    private final List<GradesExporter> gradesExporters;
 
-    public GradeExportService(GradeRepository gradeRepository) {
+    public GradeExportService(
+        GradeRepository gradeRepository,
+        List<GradesExporter> gradesExporters
+    ) {
         this.gradeRepository = gradeRepository;
+        this.gradesExporters = gradesExporters;
     }
 
     public void export(UUID courseUuid, ExportFormat exportFormat, OutputStream outputStream) {
         final List<Grade> grades = gradeRepository.findGradesByCourseUuid(courseUuid);
 
-        switch (exportFormat) {
-            case CSV: {
-                final List<List<String>> csvData = grades.stream()
-                    .map(grade -> List.of(
-                        grade.getCourseUuid().toString(),
-                        grade.getGradeType().name(),
-                        grade.getIssueDateTime().toString(),
-                        grade.getValue().toString()
-                    ))
-                    .toList();
-
-                CsvWriter.export(csvData, outputStream);
+        for (GradesExporter gradesExporter : gradesExporters) {
+            if (gradesExporter.supportsFormat(exportFormat)) {
+                gradesExporter.exportGrades(grades, outputStream);
                 return;
-            }
-
-            case STRING: {
-                PrintStream printStream = new PrintStream(outputStream);
-                grades.stream()
-                    .map(ToStringGenerator::shallowToString)
-                    .forEach(printStream::println);
-                return;
-            }
-
-            default: {
-                throw new IllegalArgumentException(exportFormat.name());
             }
         }
+
+        throw new IllegalArgumentException(exportFormat.name());
     }
 
 }
