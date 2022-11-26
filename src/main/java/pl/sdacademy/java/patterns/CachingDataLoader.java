ef8a8c1d89d09da0d86916cb1fe82d6639315b2d
Tomasz Majbaum <tomasz.majbaum@gmail.com>
2022-11-26 12:03:30 +0100

    CachingDataLoader - decorator example

---
+++ src/main/java/pl/sdacademy/java/patterns/CachingDataLoader.java
@@ -0,0 +1,22 @@
+package pl.sdacademy.java.patterns;
+
+import java.util.List;
+
+//decorator example
+public class CachingDataLoader<T> implements DataLoader<T> {
+    private final DataLoader<T> internalDataLoader;
+    private List<T> cache;
+
+    public CachingDataLoader(DataLoader<T> internalDataLoader) {
+        this.internalDataLoader = internalDataLoader;
+    }
+
+    @Override
+    public List<T> loadData() {
+        if (cache == null) {
+            cache = internalDataLoader.loadData();
+        }
+
+        return List.copyOf(cache);
+    }
+}
---
+++ src/test/java/pl/sdacademy/java/patterns/CachingDataLoaderTest.java
@@ -0,0 +1,38 @@
+package pl.sdacademy.java.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class CachingDataLoaderTest {
+
+    @Mock
+    private DataLoader<String> mockDataLoader;
+
+    @Test
+    void shouldLoadDataOnce() {
+        //given
+        given(mockDataLoader.loadData()).willReturn(List.of("foo"));
+
+        CachingDataLoader<String> cachingDataLoader = new CachingDataLoader<>(mockDataLoader);
+
+        //when
+        List<String> data1 = cachingDataLoader.loadData();
+        List<String> data2 = cachingDataLoader.loadData();
+
+        //then
+        assertThat(data1).containsExactly("foo");
+        assertThat(data2).containsExactly("foo");
+
+        verify(mockDataLoader, times(1)).loadData();
+    }
+}
